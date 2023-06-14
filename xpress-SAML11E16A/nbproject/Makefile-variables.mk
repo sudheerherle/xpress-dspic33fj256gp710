f69b0d9555b2cc04675ec1d1bea0fdbcafaf9fa0
Sudheer Herle <ssherle89@gmail.com>
2023-06-14 15:31:56 +0530

    initial commit

---
+++ xpress-SAML11E16A/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=xpress-SAML11E16A.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/xpress-SAML11E16A.production.hex

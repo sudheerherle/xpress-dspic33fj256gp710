66963b3f6d1333be10fba7430a10c9173f95d552
sudheer.herle@microchip.com <sudheer.herle@microchip.com>
2023-08-15 04:32:16 +0000

    updated size

--- 1692064787862/dspic33fj256gp710/newmain.c
+++ 1692064787862/dspic33fj256gp710/newmain.c
@@ -18,11 +18,16 @@ int main(int argc, char** argv) {
     int d = 12;
     int e = 15;
     int f = 0;
+    char a;
+    char b;
+    char myString1[20000] = "this is me";
     f = add(d,e);
     while(1){
     f = f * 2;   
     for(int p=0;p<10;p++){
         f = f + 1;
+        a = myString1[p];
+        b = myString1[p+1];
     }
     // return (EXIT_SUCCESS);
     }
@@ -30,6 +35,8 @@ int main(int argc, char** argv) {
 
 int add(int a, int b){
     int c = 0;
+    char m;
     c = a + b;
+    char myString2[20000] = "this is me";
     return c;
 }
